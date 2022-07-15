2e1c369ec0837d1246cc7d525c18510bb8f0565e
Maxim <cuwuless@gmail.com>
2022-07-15 18:09:23 +0400

    asdf

--- main.py
+++ main.py
@@ -18,42 +18,38 @@ ACR_BEARER = os.environ.get('ACR_BEARER')
 
 @dp.message_handler(filters.CommandStart())
 async def start(message: types.Message):
-    if message.from_user.id != 1999113390:
-        await message.reply("Пока")
-        return
-    else:    
-        await message.reply(f"*🔥 Привет! Я бот для парсинга релизов с белива\nНачинаю постинг релизов в канал с ID `{RELEASES_CHANNEL}`\n🧑‍💻 Разработчик: @clownl3ss*", parse_mode="markdown")
-        for upc in range(3616849174566, 999999999999999999):
-            requrl = f'http://player.believe.fr/v2/{upc}'
-            coverurl = f'https://covers.believedigital.com/full/{upc}.jpg'
-            response = requests.get(requrl)
-            soup = BeautifulSoup(response.text, "html.parser")
-            title = soup.find("meta", property="og:title")['content']
-            author = soup.find("meta", attrs={'name':'author'})['content']
-            if author:
-                acrrequrl = f"https://eu-api-v2.acrcloud.com/api/music/search?per_page=1&page=1&q=upc:{upc}"
-                headers = {
-                'Accept': 'application/json',
-                'Authorization': 'Bearer {ACR_BEARER}'
-                }
-                response = requests.get(acrrequrl, headers=headers)
-                data = json.loads(response.text)
-                try:
-                    date = data["data"][0]["release_date"]
-                except:
-                    date = "None (нету в акре)"
-                try:    
-                    album = data["data"][0]["album"]["name"]
-                except:
-                    album = "None (нету в акре)"
-                try:
-                    label = data["data"][0]["label"]
-                except:
-                    label = "None (нету в акре)"
-                try:
-                    await bot.send_photo(RELEASES_CHANNEL, coverurl, f'{author} - {title}\n\nДата релиза: {date}\nАльбом: {album}\nUPC: {upc}\nЛейбл: {label}')
-                except:
-                    pass
+    await message.reply(f"*🔥 Привет! Я бот для парсинга релизов с белива\nНачинаю постинг релизов в канал с ID `{RELEASES_CHANNEL}`\n🧑‍💻 Разработчик: @clownl3ss*", parse_mode="markdown")
+    for upc in range(3616849174566, 999999999999999999):
+        requrl = f'http://player.believe.fr/v2/{upc}'
+        coverurl = f'https://covers.believedigital.com/full/{upc}.jpg'
+        response = requests.get(requrl)
+        soup = BeautifulSoup(response.text, "html.parser")
+        title = soup.find("meta", property="og:title")['content']
+        author = soup.find("meta", attrs={'name':'author'})['content']
+        if author:
+            acrrequrl = f"https://eu-api-v2.acrcloud.com/api/music/search?per_page=1&page=1&q=upc:{upc}"
+            headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {ACR_BEARER}'
+            }
+            response = requests.get(acrrequrl, headers=headers)
+            data = json.loads(response.text)
+            try:
+                date = data["data"][0]["release_date"]
+            except:
+                date = "None (нету в акре)"
+            try:    
+                album = data["data"][0]["album"]["name"]
+            except:
+                album = "None (нету в акре)"
+            try:
+                label = data["data"][0]["label"]
+            except:
+                label = "None (нету в акре)"
+            try:
+                await bot.send_photo(RELEASES_CHANNEL, coverurl, f'{author} - {title}\n\nДата релиза: {date}\nАльбом: {album}\nUPC: {upc}\nЛейбл: {label}')
+            except:
+                pass
 
 
 if __name__ == '__main__':
