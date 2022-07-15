ef16deab4d82e70e412348ecaf16ce349388f45c
Maxim <cuwuless@gmail.com>
2022-07-15 19:58:00 +0400

    Update main.py

--- main.py
+++ main.py
@@ -22,43 +22,46 @@ pattern = re.compile(regex)
 
 @dp.message_handler(filters.CommandStart())
 async def start(message: types.Message):
-    await message.reply(f"*🔥 Привет! Я бот для парсинга релизов с белива\nНачинаю постинг релизов в канал с ID `{RELEASES_CHANNEL}`\n🧑‍💻 Разработчик: @clownl3ss*", parse_mode="markdown")
-    for upc in range(3616849311725, 999999999999999999):
-        requrl = f'http://player.believe.fr/v2/{upc}'
-        coverurl = f'https://covers.believedigital.com/full/{upc}.jpg'
-        response = requests.get(requrl)
-        soup = BeautifulSoup(response.text, "html.parser")
-        title = soup.find("meta", property="og:title")['content']
-        author = soup.find("meta", attrs={'name':'author'})['content']
-        if author:
-            acrrequrl = f"https://eu-api-v2.acrcloud.com/api/music/search?per_page=1&page=1&q=upc:{upc}"
-            headers = {
-            'Accept': 'application/json',
-            'Authorization': f'Bearer {ACR_BEARER}'
-            }
-            response = requests.get(acrrequrl, headers=headers)
-            data = json.loads(response.text)
-            firstline = f'{author} - {title}'
-            antiarab = pattern.search(firstline) is not None
-            try:
-                date = data["data"][0]["release_date"]
-            except:
-                date = "None (нету в акре)"
-            if date == "":
-                date = "None (нету в акре)"
-            try:    
-                album = data["data"][0]["album"]["name"]
-            except:
-                album = "None (нету в акре)"
-            try:
-                label = data["data"][0]["label"]
-            except:
-                label = "None (нету в акре)"
-            try:
-                if antiarab == False:
-                    await bot.send_photo(RELEASES_CHANNEL, coverurl, f'{firstline}\n\nДата релиза: {date}\nАльбом: {album}\nUPC: {upc}\nЛейбл: {label}')
-            except:
-                pass
+    if message.from_user.id != 1999113390:
+        return
+    elif message.from_user.id == 1999113390:
+        await message.reply(f"*🔥 Привет! Я бот для парсинга релизов с белива\nНачинаю постинг релизов в канал с ID `{RELEASES_CHANNEL}`\n🧑‍💻 Разработчик: @clownl3ss*", parse_mode="markdown")
+        for upc in range(3616849311725, 999999999999999999):
+            requrl = f'http://player.believe.fr/v2/{upc}'
+            coverurl = f'https://covers.believedigital.com/full/{upc}.jpg'
+            response = requests.get(requrl)
+            soup = BeautifulSoup(response.text, "html.parser")
+            title = soup.find("meta", property="og:title")['content']
+            author = soup.find("meta", attrs={'name':'author'})['content']
+            if author:
+                acrrequrl = f"https://eu-api-v2.acrcloud.com/api/music/search?per_page=1&page=1&q=upc:{upc}"
+                headers = {
+                'Accept': 'application/json',
+                'Authorization': f'Bearer {ACR_BEARER}'
+                }
+                response = requests.get(acrrequrl, headers=headers)
+                data = json.loads(response.text)
+                firstline = f'{author} - {title}'
+                antiarab = pattern.search(firstline) is not None
+                try:
+                    date = data["data"][0]["release_date"]
+                except:
+                    date = "None (нету в акре)"
+                if date == "":
+                    date = "None (нету в акре)"
+                try:    
+                    album = data["data"][0]["album"]["name"]
+                except:
+                    album = "None (нету в акре)"
+                try:
+                    label = data["data"][0]["label"]
+                except:
+                    label = "None (нету в акре)"
+                try:
+                    if antiarab == False:
+                        await bot.send_photo(RELEASES_CHANNEL, coverurl, f'{firstline}\n\nДата релиза: {date}\nАльбом: {album}\nUPC: {upc}\nЛейбл: {label}')
+                except:
+                    pass
 
 
 if __name__ == '__main__':
